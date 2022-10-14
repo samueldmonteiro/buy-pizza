08bc0d44f9e88f74e4d26a3be153f72c96e411dc
Samuel Monteiro <samuel.dvmonteiro@gmail.com>
2022-10-14 02:11:41 -0300

    configuring template

---
+++ js/app.js
@@ -0,0 +1,4 @@
+import { qs, qsAll } from "./modules/utils.js";
+import { data } from "./modules/data.js";
+
+console.log(data);
---
+++ js/modules/utils.js
@@ -0,0 +1,9 @@
+// alias for querySelector / all
+
+export const qs = element => {
+    return document.querySelector(element);
+}
+
+export const qsAll = element => {
+    return document.querySelectorAll(element);
+}
